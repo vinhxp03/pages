facb006c3cd768f31ca6a79281c2dceb64e83b9d
Vinh Phan <vinhxp03@gmail.com>
2020-11-14 01:33:16 +0700

    Automate commit 2020-10-14 1:33:13

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 const execSync = require('child_process').execSync;
 
 const d = new Date();
-const dateTime = `${d.getFullYear}-${d.getMonth()}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+const dateTime = `${d.getFullYear()}-${d.getMonth()}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
 
 execSync(`git pull origin master && git add . && git commit -m "Automate commit ${dateTime}" && git push origin master`);
